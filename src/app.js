d4ff48b89dacd9bf9c05e168c70369ac9ebec0e7
NileshNandaniya <nileshnandaniya135@gmail.com>
2020-10-30 17:39:01 +0530

    Setup for deployment

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const app = express()
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
+const port = process.env.PORT || 3000
+
 //define express path config
 const publicDirectory = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -95,8 +97,8 @@ app.get( '*' , (req, res)=>{
         name:'Nilesh'        
     })
 })
-app.listen(3000, ()=>{
-    console.log('Server is running....')
+app.listen(port, ()=>{
+    console.log('Server is running on port ' + port)
 })
 
 
